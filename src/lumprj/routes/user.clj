c8e16e4146377127d292d7b84566e191c354b99a
hongmomanu <hongmomanu@gmail.com>
2013-12-18 12:09:20 +0800

    jack commit

--- src/lumprj/routes/user.clj
+++ src/lumprj/routes/user.clj
@@ -26,26 +26,10 @@
        (let [results (db/has-user  username password)]
 
 
-         ;;(true? (> (:num results) 0) (print "yes") (print "no") )
         ( if (> (count results) 0) (resp/json {:success true }) (resp/json {:success false}))
 
          )
 
-       ;;(resp/jsonp "showUsers" {:username name :pass passwd})
-       ;;(json {:response "ok"})
-
-       )
-
-  (POST "/ajaxlogin" [name,passwd]
-       (println name)
-       (println passwd)
-       (resp/json (first (db/has-user  name passwd)))
-
-       )
-
-  (POST "/logintest" [name,passwd]
-
-       (resp/json {:success true :result {:userDesktop {:launchers {} :modules {} :style {:wallpaper {}}}}})
 
        )
 
