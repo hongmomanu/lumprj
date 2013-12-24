5f5a0199ac05ff9ee71c495deac6c9e475f7a3c2
hongmomanu <hongmomanu@gmail.com>
2013-12-24 15:54:36 +0800

    jack commit

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :dependencies
   [[com.h2database/h2 "1.3.174"]
    [org.xerial/sqlite-jdbc "3.7.2"]
+   [com.oracle/ojdbc6 "11.2.0.3"]
    [ring-server "0.3.1"]
    [lein-ring "0.8.8"]
    [environ "0.4.0"]
@@ -40,6 +41,30 @@
   :all
   :plugins
   [[lein-ring "0.8.8"] [lein-environ "0.4.0"] [ring-refresh "0.1.1"][ring/ring-jetty-adapter "1.2.1"]]
+  :repositories [
+                  ["java.net" "http://download.java.net/maven/2"]
+                  ["nexus" "https://code.lds.org/nexus/content/groups/main-repo"]
+                 ["sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                              ;; If a repository contains releases only setting
+                              ;; :snapshots to false will speed up dependencies.
+                              :snapshots false
+                              ;; Disable signing releases deployed to this repo.
+                              ;; (Not recommended.)
+                              :sign-releases false
+                              ;; You can also set the policies for how to handle
+                              ;; :checksum failures to :fail, :warn, or :ignore.
+                              :checksum :fail
+                              ;; How often should this repository be checked for
+                              ;; snapshot updates? (:daily, :always, or :never)
+                              :update :always
+                              ;; You can also apply them to releases only:
+                              :releases {:checksum :fail :update :always}}]
+                 ;; Repositories named "snapshots" and "releases" automatically
+                 ;; have their :snapshots and :releases disabled as appropriate.
+                 ;; Credentials for repositories should *not* be stored
+                 ;; in project.clj but in ~/.lein/credentials.clj.gpg instead,
+                 ;; see `lein help deploying` under "Authentication".
+                 ]
   :description
   "FIXME: write description"
   :min-lein-version "2.0.0")
